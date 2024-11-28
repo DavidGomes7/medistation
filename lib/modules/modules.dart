dac8417a7f048a3bdb5c85edf15c264e97ae9177
andrezin96 <andreantoniosilva96@gmail.com>
2024-11-27 21:05:19 -0300

    Configuração inicial

---
+++ lib/modules/modules.dart
@@ -0,0 +1,6 @@
+export 'auth/auth.dart';
+export 'exercise/exercise.dart';
+export 'goal/goal.dart';
+export 'home/home.dart';
+export 'profile/profile.dart';
+export 'tips/tips.dart';
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:medistation/main.dart';
+import 'package:medistation/medi_station.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const MediStation());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
