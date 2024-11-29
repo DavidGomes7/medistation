9b3a3db5a98f4d9a958d9b7e1b8ddf48d276f8ba
Douglas A Silva <douglaseDeus13@gmail.com>
2024-11-29 00:52:48 -0300

    Criando o Drawer

--- lib/modules/home/components/home_template.dart
+++ lib/modules/home/components/home_template.dart
@@ -7,7 +7,27 @@ class HomeTemplate extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(''),
+        title: const Text('MediStation'),
+        centerTitle: true,
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      drawer: const Drawer(
+        child: Padding(
+          padding: EdgeInsets.all(16),
+          child: SafeArea(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CircleAvatar(
+                  radius: 40,
+                  child: Icon(Icons.person),
+                ),
+                Text('Douglas Silva'),
+                Text('example@gmail.com'),
+              ],
+            ),
+          ),
+        ),
       ),
       body: Container(),
     );
