6d3dfec834aafa40da4982e1eb58a5876b389937
andrezin96 <andreantoniosilva96@gmail.com>
2024-11-29 19:05:20 -0300

    Ajuste de instância

--- lib/interfaces/medi_injector/medi_injector.dart
+++ lib/interfaces/medi_injector/medi_injector.dart
@@ -1,4 +1,8 @@
+import 'get_it_impl.dart';
+
 abstract class MediInjector {
   T get<T extends Object>();
   void replace<T extends Object>(T instance);
 }
+
+final MediInjector injector = GetItImpl();
